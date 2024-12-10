e4df098b1f12d239ed877042a21ed138aa1bd31d
RiccardoBianchi99 <bianchi.ricky1999@gmail.com>
2024-12-10 18:16:38 +0100

    Update NeuralNetworkLibrary.py

--- NeuralNetworkLibrary.py
+++ NeuralNetworkLibrary.py
@@ -6,55 +6,76 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import time
 
+# ReLU function, used as activation function in the hidden layers
 def ReLU(Z):
     # This will be applied to each element of Z
     return np.maximum(0, Z)
 
+# Softmax function, used as actionvation function in the last layer of our NN
+# Sofrmax function converts quantities from a whide range of values into probabilities
 def softmax(Z):
     e_x = np.exp(Z - np.max(Z))
     return e_x / e_x.sum(axis=0)
 
+# One hot encoding function, used to compare the prediction of our NN after the softmax convertin.
+# This function is used to convert a quantity in 1x1 dimension into a 1xM dimension, where M is the number of values that we can predict.
 def one_hot(Y):
     one_hot_Y = np.zeros((Y.size, int(Y.max() + 1)))
     one_hot_Y[np.arange(Y.size), Y] = 1
     one_hot_Y = one_hot_Y.T
     return one_hot_Y
 
+# Derivative of the ReLU function used in the backward propagation to update the weight of our NN.
 def deriv_ReLU(Z):
     return Z>0
 
+# Convertion from the softmax representation into a single value prediction.
 def get_predictions(A):
     return np.argmax(A, 0)
 
+# Computes the discrepancies between predictions and true solution
 def get_accuracy(predictions, Y):
     return np.sum(predictions == Y) / Y.size
 
+# Definition of the class NeuralNetwork. This contains all the function needed to create, update and test a neural network.
+# The NN created will be a fully connected neural network.
 class NeuralNetwork:
+    
+    # The initializers need the size of the inputs, of the outputs and a vector that describe the number of layers and preceptrons
     def __init__(self, layers, size_input = 784, size_output = 10, seed = 10 ):
         
+        # We assume at least two layers in our neural network
         if len(layers) < 2:
             print(f'!! Insert at least TWO hidden layer in the neural network !!')
             layers = [10, 10]
-            
+        
+        # The last layers works as the output layer basically, since its output are given to the softmax function
         if layers[-1] != size_output:
             print(f'!! The size of the last layer has to be the same of the output ({size_output}) !!')
             layers[-1] = size_output
         
+        # Create the basic properties of the neural network
         self.layers = layers
         self.num_layers = len(layers)
         self.size_input = size_input
         self.size_output = size_output
         
+        # W and b represents the weights and the bias of each connection of our network
         self.W, self.b = self.init_params(seed = seed)
     
     def init_params(self, seed):
+        # All values of the weight and bias are initialized between -0.5 and 0.5 
         
+        # W and b represent the list of matrix containing the weights for each layer
         W = []
         b = []
         np.random.seed(seed)
+        
+        # The first layer connects all the inputs to all the perceptrons
         W.append( np.random.rand(self.layers[0], self.size_input) - 0.5)
         b.append(np.random.rand(self.layers[0], 1) - 0.5)
         
+        # The rest connect the number of perceptrons from the previous layer to the current
         for i in range(self.num_layers - 1):
             W.append( np.random.rand(self.layers[i+1], self.layers[i]) - 0.5)
             b.append(np.random.rand(self.layers[i+1], 1) - 0.5)
